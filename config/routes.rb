755a9b09c3b5490dde354da2bedd48c34fbf9005
Rich Shea <richcshea@gmail.com>
2015-04-21 16:27:32 -0600

    add footer to application and center

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root 'static_pages#home'
+  get 'static_pages/home', to: "static_pages#home"
+  get 'static_pages/landing', to: "static_pages#landing"
+  root 'static_pages#landing'
 
   resources :users, exclude: [:index]
 end
