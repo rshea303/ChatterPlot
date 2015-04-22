94da836aa5d6abf3ad048c35e8235a08bae19ab0
Rich Shea <richcshea@gmail.com>
2015-04-22 13:43:46 -0600

    add logout link when no current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authorize!
+  #before_action :authorize!
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
