823816afdd861ea4c6371ba5cafa522f3e9ef12d
Rich Shea <richcshea@gmail.com>
2015-04-25 14:21:18 -0600

    add seed data

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ActiveRecord::Base
+  has_many :sentiments
+  has_many :stock_prices
+
   validates :name, :ticker_symbol, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+
+  def call
+    sentiments
+  end
+
+  def companies
+    [ 
+    Company.create(name: "google", ticker_symbol: "GOOG"),
+    Company.create(name: "apple", ticker_symbol: "APPL"),
+    Company.create(name: "yahoo", ticker_symbol: "YHOO"),
+    Company.create(name: "facebook", ticker_symbol: "FB"),
+    Company.create(name: "ibm", ticker_symbol: "IBM")
+    ]
+  end
+
+  def sentiments
+    companies.each do |company|
+      sentiment = company.sentiments.create(date_and_time: DateTime.now, score: rand(25..75))
+      puts "Company: #{company.name} (#{company.ticker_symbol})  --- Sentiment: date_and_time: #{sentiment.date_and_time} score: #{sentiment.score}"
+      stock_price = company.stock_prices.create(date_and_time: DateTime.now, price: rand(1000..50000))
+      puts "Company: #{company.name} (#{company.ticker_symbol})  --- Stock Price: date_and_time: #{stock_price.date_and_time} score: #{stock_price.price}"
+    end
+  end
+
+  def self.call
+    new.call
+  end
+
+end
+
+Seed.call
