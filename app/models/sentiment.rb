02b46c3b6f08af8adaa51eb6fa3b0b4d74a7b1e6
Rich Shea <richcshea@gmail.com>
2015-04-25 10:54:42 -0600

    add validation tests for sentiment

--- app/models/sentiment.rb
+++ app/models/sentiment.rb
@@ -1,3 +1,5 @@
 class Sentiment < ActiveRecord::Base
   belongs_to :company
+
+  validates :date_and_time, :score, :company_id, presence: true
 end
--- spec/models/sentiment_spec.rb
+++ spec/models/sentiment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Sentiment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with date_and_time, score, and company_id" do
+    sentiment = Sentiment.new(date_and_time: DateTime.now, score: 50, company_id: 1)
+
+    expect(sentiment).to be_valid
+  end
+
+  it "is not valid without a date_and_time" do
+    sentiment = Sentiment.new(date_and_time: nil, score: 50, company_id: 1)
+    
+    expect(sentiment).not_to be_valid
+  end
+
+  it "is not valid without a score" do
+    sentiment = Sentiment.new(date_and_time: DateTime.now, score: nil, company_id: 1)
+
+    expect(sentiment).not_to be_valid
+  end
+
+  it "is not valid without a company_id" do
+    sentiment = Sentiment.new(date_and_time: DateTime.now, score: 50, company_id: nil)
+
+    expect(sentiment).not_to be_valid
+  end
 end
+
