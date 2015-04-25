a1acbe387f0e432acbb94687fa012178ecf17866
Rich Shea <richcshea@gmail.com>
2015-04-25 09:50:15 -0600

    add company model and companies controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422174230) do
+ActiveRecord::Schema.define(version: 20150425154822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "ticker_symbol"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "nickname"
