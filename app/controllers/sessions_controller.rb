bbec28732679184e5a9b50fbbd09f29ba89ffc5f
Rich Shea <richcshea@gmail.com>
2015-04-27 07:30:01 -0600

    change http to https and fix sessions destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    session.delete[:user_id]    
+    @current_user = nil
     redirect_to root_path
   end
 end
