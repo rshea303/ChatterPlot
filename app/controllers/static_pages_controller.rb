bf74a660d1962df6d5dadb909a4a682da022fc17
Rich Shea <richcshea@gmail.com>
2015-04-29 14:15:56 -0600

    destroy home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,23 @@
 class StaticPagesController < ApplicationController
   def home
-    @company_short ||= params[:company].upcase if params[:company]
+    if params[:company]
+      @company = Company.find_by(name: params[:company])
 
-    if @company_short == "MSFT"
-      @data_points = [1,2,3,4,5,6,7,8,9,1]
-    elsif @company_short = "GOOG"
-      @data_points = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+      if @company.name == "facebook"
+        @data_points = [1,2,3,4,5,6,7,8,9,1]
+      elsif @company.name == "google"
+        @data_points = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+      else
+        @data_points = [1, 2, 5, 78, 56, 34, 23, 56, 78, 90]
+      end
+
     else
-      @data_points = [1, 2, 5, 78, 56, 34, 23, 56, 78, 90] 
+      params[:company] = nil
     end
-
   end
 
   def landing
+  
   end
 end
 
