ed5ff630f89b48e81501b4b3200a88c7ad913e59
Rich Shea <richcshea@gmail.com>
2015-04-29 07:44:18 -0600

    add dynamic data to bar graph

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    @company_short ||= params[:company].upcase if params[:company]
+
+    if @company_short == "MSFT"
+      @data_points = [1,2,3,4,5,6,7,8,9,1]
+    elsif @company_short = "GOOG"
+      @data_points = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    else
+      @data_points = [1, 2, 5, 78, 56, 34, 23, 56, 78, 90] 
+    end
+
   end
 
   def landing
