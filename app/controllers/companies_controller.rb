97ce1e45865a866505073abe19e3d38e44248f90
Rich Shea <richcshea@gmail.com>
2015-04-30 16:24:18 -0600

    show page renders articles from yahoo finance

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,7 @@
 class CompaniesController < ApplicationController
   def show
     @company = Company.find_by(name: params[:q])
+    @articles = YahooService.new.articles(@company.ticker_symbol)
   end
   
 end
--- app/services/yahoo_service.rb
+++ app/services/yahoo_service.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 
 class YahooService
-  attr_reader :connection
+  attr_reader :connection, :data, :article_info
 
   def initialize
     @connection = Faraday.new(url: 'http://finance.yahoo.com/rss/2.0')
@@ -9,10 +9,20 @@ class YahooService
 
   def articles(ticker)
     params = { s: ticker, region: 'US', lang: 'en-US' }
-    parse(connection.get('headline', params))
+    article_data(parse(connection.get('headline', params)))
   end 
 
   def parse(response) 
     Nokogiri::XML.parse(response.body) 
+  end
+
+  def article_data(data)
+    @article_info = data.xpath("//item").map do |item|
+       { 
+        "title"       => item.xpath("title").text,
+        "link"        => item.xpath("link").text,
+        "description" => item.xpath("description").text
+       }
+    end
   end
 end
