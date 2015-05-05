d2d2f53c200787acd7abe15d00214a7dac627256
Rich Shea <richcshea@gmail.com>
2015-05-05 17:33:35 -0600

    add recent tweets

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+    @tweets = TwitterService.new.search_by("#google").take(5).map do |tweet|
+      tweet.text
+    end
+
+    # grab tweets
+    # run tweet text thru sentiment analysis
+    # averge sentiment scores
+    # normalize the average score
+    # pass it to the graph
     @companies = Company.all
     @company_names = Company.all.map(&:name)
     @company_scores = Company.scores
