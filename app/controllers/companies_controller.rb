ee8f3f8735d2aa6ceea9afd790f7473102ba06bd
Rich Shea <richcshea@gmail.com>
2015-05-07 11:41:13 -0600

    removed comments

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -14,21 +14,6 @@ class CompaniesController < ApplicationController
     end
   end
 
-=begin
-    if Company.all.map(&:name).include?(params[:q])
-      @company = Company.find_by(name: params[:q])
-      @articles = YahooService.new.articles(@company.ticker_symbol)
-      @tweets = TwitterService.new.search_by(@company.name).take(100).map do |tweet|
-        tweet.text
-      end
-       @score = SentimentAnalysis.new(@tweets.join(' ')).score
-       @stock_price = StockInfo.new(@company.ticker_symbol).bid
-       @color = what_color(@score)
-    else
-      redirect_to :back
-    end 
-  end
-=end
   def which_color(company, stock_price)
     if stock_price > StockInfo.new(company.ticker_symbol).open
       'green'
