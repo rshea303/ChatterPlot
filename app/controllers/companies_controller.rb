1b13afe0eee3fbb9635abe6b785bca4fe7532b06
Rich Shea <richcshea@gmail.com>
2015-05-10 11:42:29 -0600

    refactor show controller for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,6 @@
 class CompaniesController < ApplicationController
   def show
-    if Company.all.map(&:name).include?(params[:q])
+    if Company.find_by(name: params[:q])
       @dashboard = DashBoard.new(Company.find_by(name: params[:q]))
     else
       redirect_to :back
