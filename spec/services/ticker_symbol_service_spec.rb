1b667745fd097e7f53e801e09aed39f678679118
Rich Shea <richcshea@gmail.com>
2015-05-17 13:58:15 -0600

    refactor api test

--- spec/services/ticker_symbol_service_spec.rb
+++ spec/services/ticker_symbol_service_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe "ticker symbol service api" do
 
+  let(:service) { TickerSymbolService.new }
+
   it "identifies company stock symbol and name" do
-    service = TickerSymbolService.new
     VCR.use_cassette('company info') do
       expect(service.symbols('exxon')['symbol']).to eq('XOM') 
       expect(service.symbols('exxon')['name']).to eq('Exxon Mobil Corporation')
