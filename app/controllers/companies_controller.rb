3366617deae3099f31c5b396ea7630dc27a730e2
Rich Shea <richcshea@gmail.com>
2015-05-17 16:47:48 -0600

    add filter for stock symbols with punctuation

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,12 @@
 class CompaniesController < ApplicationController
   def show
-    ticker_symbol = TickerSymbolService.new.symbols(params[:q])['symbol']
-    name = TickerSymbolService.new.symbols(params[:q])['name']
-    @dashboard = DashBoard.new(ticker_symbol, name)
+    ticker = TickerSymbolService.new.symbols(params[:q])
+    if ticker == nil || ticker['symbol'].chars.include?('.')
+      redirect_to :back 
+    else
+      ticker_symbol = TickerSymbolService.new.symbols(params[:q])['symbol']
+      name = TickerSymbolService.new.symbols(params[:q])['name']
+      @dashboard = DashBoard.new(ticker_symbol, name)
+    end
   end
 end
