e93b7eab96a9409bacd125767b5b92cf45866a86
Rich Shea <richcshea@gmail.com>
2015-05-17 17:33:15 -0600

    add flash message for nil company search

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,8 @@
 class CompaniesController < ApplicationController
   def show
     ticker = TickerSymbolService.new.symbols(params[:q])
-    if ticker == nil || ticker['symbol'].chars.include?('.')
+    if Company.not_legit_symbol(ticker)
+      flash[:alert] = "#{params[:q].downcase.capitalize} not found. Please check your spelling or enter a different company name."
       redirect_to :back 
     else
       ticker_symbol = TickerSymbolService.new.symbols(params[:q])['symbol']
