8eb7e468c3eefeb54a3f944e5eea44742882c3b4
Rich Shea <richcshea@gmail.com>
2015-05-30 11:53:55 -0600

    removed oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback', to: "sessions#create"
   get 'static_pages/home',        to: "static_pages#home"
   root 'static_pages#home'
-
-  resources :users, exclude: [:index]
-  resource  :session
 
   get '/search', to: "companies#show"
 end
