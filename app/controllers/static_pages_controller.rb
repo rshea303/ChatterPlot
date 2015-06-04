c8a5eced68e8c4c7b2e885b6cd43630d25266640
Rich Shea <richcshea@gmail.com>
2015-06-04 08:03:49 -0600

    company names from checklist are displaying on re-rendered bar graph

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,15 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @companies = Company.all
-    @company_names = Company.all.map(&:name).sort
-    @company_scores = Company.scores
-  end
+      @companies = Company.all
 
+    if params[:company_ids]
+      @company_names = Company.selected_companies(params[:company_ids])
+      @company_scores = Company.scores(@company_names)
+    else
+      @company_names = Company.all.map(&:name).sort
+      @company_scores = Company.scores
+    end
+  end
 end
 
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,9 +2,18 @@ class Company < ActiveRecord::Base
 
   validates :name, :ticker_symbol, presence: true
 
-  def self.scores
-    Company.all.map do |company|
-      SentimentAnalysis.new(TwitterService.new.search_by(company.name).take(100).map do |tweet|
+  def self.selected_companies(checked_companies)
+    collector = []
+    checked_companies.each do |name, id|
+      collector << name
+    end
+    collector 
+  end
+
+  def self.scores(company_names)
+    collector = []
+    company_names.each do |name|
+      collector << SentimentAnalysis.new(TwitterService.new.search_by(name).take(100).map do |tweet|
         tweet.text
       end.join(' ')).score
     end
