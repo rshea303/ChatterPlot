a6fe291f38ba257437f7709e77366c6035672571
Rich Shea <richcshea@gmail.com>
2015-06-03 19:11:38 -0600

    add instructions on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
 
   def home
-    binding.pry
     @companies = Company.all
     @company_names = Company.all.map(&:name).sort
     @company_scores = Company.scores
