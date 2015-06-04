d32ce0f6dc995f96699de500e16c1cfa4f76a39b
Rich Shea <richcshea@gmail.com>
2015-06-03 18:40:31 -0600

    add form for checkboxes on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
+    binding.pry
     @companies = Company.all
     @company_names = Company.all.map(&:name).sort
     @company_scores = Company.scores
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home',        to: "static_pages#home"
+  post 'static_pages/home',        to: "static_pages#home"
+
   root 'static_pages#home'
 
   get '/search', to: "companies#show"
